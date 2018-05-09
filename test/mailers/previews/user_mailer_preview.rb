47159571ab36a0073f26b00fd5dac7d2828c6a77
EC2 Default User <ec2-user@ip-172-31-16-79.us-east-2.compute.internal>
2018-05-09 13:57:20 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,7 +10,9 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
@@ -18,3 +20,5 @@ end
 #21b5eea18e374c01a9f07fb903bbce22.vfs.cloud9.us-east-2.amazonaws.com
 #以下のURLから
 #https://21b5eea18e374c01a9f07fb903bbce22.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/account_activation
+
+#http://21b5eea18e374c01a9f07fb903bbce22.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
